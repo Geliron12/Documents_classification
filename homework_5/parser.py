79bad65a53f508a0b746d23dde3742a3050b205c
Geliron12 <65118717+Geliron12@users.noreply.github.com>
2023-01-09 12:20:34 +0300

    Add files via upload

---
+++ homework_5/parser.py
@@ -0,0 +1,71 @@
+from telethon.sync import TelegramClient
+import csv
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.tl.functions.messages import GetHistoryRequest
+api_id = 20109403
+api_hash = 'here_was_api_hash'
+phone = '+79869675126'
+client = TelegramClient(phone, api_id, api_hash)
+client.start()
+chats = []
+last_date = None
+size_chats = 200
+groups=[]
+result = client(GetDialogsRequest(
+            offset_date=last_date,
+            offset_id=0,
+            offset_peer=InputPeerEmpty(),
+            limit=size_chats,
+            hash = 0
+        ))
+chats.extend(result.chats)
+for chat in chats:
+   try:
+       if chat.megagroup== True:
+           groups.append(chat)
+   except:
+       continue
+print('Доступные чаты:')
+i=0
+for g in groups:
+   print(str(i) + '- ' + g.title)
+   i+=1
+g_index = input("Номер нужного чата: ")
+target_group=groups[int(g_index)]
+
+all_messages = []
+offset_id = 0
+limit = 100
+total_messages = 0
+total_count_limit = 0
+
+while True:
+   history = client(GetHistoryRequest(
+       peer=target_group,
+       offset_id=offset_id,
+       offset_date=None,
+       add_offset=0,
+       limit=limit,
+       max_id=0,
+       min_id=0,
+       hash=0
+   ))
+   if not history.messages:
+       break
+   messages = history.messages
+   for message in messages:
+       all_messages.append([message.message,message.date])
+   offset_id = messages[len(messages) - 1].id
+   if total_count_limit != 0 and total_messages >= total_count_limit:
+       break
+
+print("Сохранение")
+ 
+with open("chats.csv", "w", encoding="UTF-8") as f:
+   writer = csv.writer(f, delimiter=",", lineterminator="\n")
+   writer.writerow(["message",'date'])
+   for message in all_messages:
+       writer.writerow([message[0],message[1]])     
+
+print("Парсинг окончен")
